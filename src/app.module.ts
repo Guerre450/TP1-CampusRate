5973088c8b564211f44eff42a117c034f46b875e
cedric <cedric>
2026-09-13 09:26:58 -0400

    add ratings and refactored base generated module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PlacesModule } from './places/places.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PlacesModule } from './places/places.module';
       isGlobal: true,
     }),
     PlacesModule,
+    RatingsModule,
   ],
   controllers: [],
   providers: [],
