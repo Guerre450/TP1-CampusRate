2c2d075429610879afcbd3557be8f70200b1098e
cedric <cedric>
2026-09-13 10:12:07 -0400

    fix updateAt not getting updated

--- src/places/places.service.ts
+++ src/places/places.service.ts
@@ -89,7 +89,7 @@ export class PlacesService implements OnModuleInit, OnModuleDestroy {
           value: id,
         },
       ],
-      {...updatePlaceDto, updatedAt : new Date()},
+      { ...updatePlaceDto, updatedAt: new Date() },
     );
     if (!result.successful) {
       throw new BadRequestException(
