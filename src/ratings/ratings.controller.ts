27fd5600c3d0272a4f162082169966e08764a6d6
cedric <cedric>
2026-09-14 09:42:23 -0400

    add partial swagger

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -12,25 +12,77 @@ import {
 import { RatingsService } from './ratings.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNoContentResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { ProblemDetailsDto } from 'src/common/exception/problem-details.dto';
+import { Rating } from './entities/rating.entity';
 
+@ApiTags('Ratings')
+@ApiBadRequestResponse({
+  description: 'invalid data',
+  type: ProblemDetailsDto
+})
 @Controller('ratings')
 export class RatingsController {
-  constructor(private readonly ratingsService: RatingsService) {}
+  constructor(private readonly ratingsService: RatingsService) { }
 
+
+  @ApiOperation({
+    summary: "Find a rating",
+    description: "Find a rating using a id"
+  })
+  @ApiParam(
+    {
+      name: 'id',
+      description: "uuid of the rating",
+      format: "uuid"
+    })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.ratingsService.findOne(id);
   }
+  @ApiOperation({
+    summary: "List ratings of a place",
+    description: "list ratings related to a placeId"
+  })
+  @ApiParam(
+    {
+      name: 'placeId',
+      description: "uuid of the place",
+      format: "uuid"
+    }
+  )
   @Get('places/:placeId')
   async findAllOfPlace(@Param('placeId') placeId: string) {
     return await this.ratingsService.findAll(placeId);
   }
-
+  @ApiOperation({
+    summary: "Creates a rating",
+    description: "Creates a rating using a placeId"
+  })
+  @ApiCreatedResponse({
+    description: "The created rating",
+  })
   @Post()
   async create(@Body() createRatingDto: CreateRatingDto) {
     return await this.ratingsService.create(createRatingDto);
   }
 
+  @ApiOperation({
+    summary : "Updates the rating",
+    description : "Updates the rating with it's id "
+  })
+  @ApiOkResponse(
+    {
+      description : "Returns the updated rating"
+    }
+  )
+  @ApiParam(
+    {
+      name : 'id',
+      description: "uuid of the rating to update",
+      format : 'uuid'
+    }
+  )
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -38,6 +90,19 @@ export class RatingsController {
   ) {
     return await this.ratingsService.update(id, updateRatingDto);
   }
+
+  @ApiOperation({
+    summary : 'Delete rating by id',
+    description : "Finds a rating by it's id and deletes it"
+  })
+  @ApiNoContentResponse({
+    description : "No content is displayed for deletion operation"
+  })
+  @ApiParam({
+    name : 'id',
+    description : 'uuid of the rating',
+    format : 'uuid'
+  })
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   async remove(@Param('id') id: string) {
---
+++ src/startup-configurations/configure-swagger.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function configureSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Energy API')
+    .setDescription(
+      'Energy gestion building project API REST.',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'docs/openapi.json',
+    customSiteTitle: 'Energy API - Documentation',
+  });
+}
